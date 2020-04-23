458f912fc223ab0536cfdecdb936e7c1f4b3973d
saberla <1850065377@qq.com>
2020-04-24 00:35:34 +0800

    领导大屏echarts接口

--- routes/api/logRecord.js
+++ routes/api/logRecord.js
@@ -4,6 +4,11 @@ const router = express.Router()
 const passport = require('passport')
 
 const Records = require('../../Schemas/logRecordShema') // 记录库
+const Houses = require('../../Schemas/houseSchema') //房屋库
+const Communities = require('../../Schemas/communitySchema') // 小区库
+const Persons = require('../../Schemas/personSchema') //人员库
+const Users = require('../../Schemas/userSchema') // user数据库
+const Cars = require('../../Schemas/carSchema') //车辆库
 
 // 写入登录记录 请求会带数据 POST请求
 // api/records/writeRecords
@@ -45,5 +50,63 @@ router.post('/getRecords', passport.authenticate('jwt', {session:false}), (req, 
     }
   })
 })
+
+// 返回echarts图表信息 POST请求
+// passport 验证tocken
+// private
+// api/records/getTotals
+router.post('/getTotals',passport.authenticate('jwt', {session:false}) , (req, res) => {
+    let  arr = []
+  Houses.countDocuments({}, (err, count) => {
+      if (err) {res.json({data: {code: 400, msg: `${JSON.stringify(err)}`}})}
+      else {
+        let obj = {}
+        obj.name = '房屋总数'
+        obj.value = count
+        arr.push(obj)
+        Persons.countDocuments({}, (err, count1) => {
+          if (err) {res.json({data: {code: 400, msg: `${JSON.stringify(err)}`}})}
+          else {
+            let obj = {}
+            obj.name = '人员总数'
+            obj.value = count1
+            arr.push(obj)
+            Communities.countDocuments({}, (err, count2) => {
+              if (err) {res.json({data: {code: 400, msg: `${JSON.stringify(err)}`}})}
+              else {
+                let obj = {}
+                obj.name = '小区总数'
+                obj.value = count2
+                arr.push(obj)
+                Users.countDocuments({}, (err, count3) => {
+                  if (err) {res.json({data: {code: 400, msg: `${JSON.stringify(err)}`}})}
+                  else {
+                    let obj = {}
+                    obj.name = '管理员总数'
+                    obj.value = count3
+                    arr.push(obj)
+                    Cars.countDocuments({}, (err, count4) => {
+                      if (err) {res.json({data: {code: 400, msg: `${JSON.stringify(err)}`}})}
+                      else {
+                        let obj = {}
+                        obj.name = '车辆总数'
+                        obj.value = count4
+                        arr.push(obj)
+                        res.json({data:{
+                          code: 200,
+                          totalRes: arr
+                        }})
+                      }
+                    })
+                  }
+                })
+              }
+            })
+          }
+        })
+      }
+  })
+})
+
 
 module.exports = router
